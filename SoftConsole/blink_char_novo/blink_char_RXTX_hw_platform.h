7f38e26157565d2d66207e490275b4646e7c2c6f
Clara Salles <95931755+clarasalless@users.noreply.github.com>
2024-07-22 16:03:32 -0300

    pelo amor de deus não quebra

---
+++ SoftConsole/blink_char_novo/blink_char_RXTX_hw_platform.h
@@ -0,0 +1,16 @@
+#ifndef blink_char_RXTX_HW_PLATFORM_H_
+#define blink_char_RXTX_HW_PLATFORM_H_
+/*****************************************************************************
+*
+*Created by Microsemi SmartDesign  Tue Jul  9 17:29:50 2024
+*
+*Memory map specification for peripherals in blink_char_RXTX
+*/
+
+/*-----------------------------------------------------------------------------
+* CM3 subsystem memory map
+* Initiator(s) for this subsystem: CM3 
+*---------------------------------------------------------------------------*/
+
+
+#endif /* blink_char_RXTX_HW_PLATFORM_H_*/
